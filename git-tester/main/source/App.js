f1e1f2568917f8a10a44b4134db8623bfe46056a
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-10-31 09:35:17 -0700

    working in control for hashrouter

--- git-tester/main/source/App.js
+++ git-tester/main/source/App.js
@@ -1,40 +1,39 @@
 import React from 'react';
 import GetBranches from './GetBranches';
 import YouRang from './YouRang';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Intro from './Intro';
 
 export default class App extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Intro />
-                <Router>
-                    <div>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <Link to="/">Home</Link>
-                                </li>
-                                <li>
-                                    <Link to="/you-rang">You Rang</Link>
-                                </li>
-                                <li>
-                                    <Link to="/get-branches">Get Branches</Link>
-                                </li>
-                            </ul>
-                        </nav>
+                <Intro/>
 
-                        <Switch>
-                            <Route path="/get-branches">
-                                <GetBranches />
-                            </Route>
-                            <Route path="/you-rang">
-                                <YouRang />
-                            </Route>
-                        </Switch>
-                    </div>
-                </Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/you-rang">You Rang</Link>
+                            </li>
+                            <li>
+                                <Link to="/get-branches">Get Branches</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Switch>
+                        <Route path="/get-branches">
+                            <GetBranches/>
+                        </Route>
+                        <Route path="/you-rang">
+                            <YouRang/>
+                        </Route>
+                    </Switch>
+                </div>
             </React.Fragment>
         );
     }
