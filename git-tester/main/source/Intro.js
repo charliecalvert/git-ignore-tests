1361a56329efee5afa4aca06d1b407b4268a03ae
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-11-05 12:33:17 -0800

    React Hot Reloading Hooks

--- git-tester/main/source/Intro.js
+++ git-tester/main/source/Intro.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Intro() {
     return (
         <React.Fragment>
-            <h1>Main Calvert</h1>
+            <h1>React Hooks Git Ignore Tests</h1>
 
-            <p>Midterm prototype code with function components and hooks.</p>
+            <p>Midterm prototype code with function components and hooks with Docker hot reloading.</p>
 
             <h2>Menu</h2>
         </React.Fragment>
