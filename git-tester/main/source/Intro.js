bca5f91ae07ea8c6263512f54de3dfa7b4341950
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-11-05 10:48:17 -0800

    main/source/Intro updated

--- git-tester/main/source/Intro.js
+++ git-tester/main/source/Intro.js
@@ -4,9 +4,9 @@ export default class Intro extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <h1>Main Calvert</h1>
+                <h1>Git Ignore Tests Router Dom</h1>
 
-                <p>Midterm prototype code.</p>
+                <p>Midterm prototype code with React Router DOM and Docker Hot Reloading.</p>
 
                 <h2>Menu</h2>
             </React.Fragment>
